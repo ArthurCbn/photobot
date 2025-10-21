c88337bd6c1d9a61f6fff76029557bdf7a03dc31
Arthur Cabon <djarth2222@gmail.com>
2025-10-21 16:58:46 +0200

    feat: init data folder

--- src/parameters.py
+++ src/parameters.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+import os
 
 SRC_PATH = Path(__file__).absolute().parent
 REPO_PATH = SRC_PATH.parent
 DATA_PATH = REPO_PATH / "data"
+
+if not DATA_PATH.exists() :
+    os.makedirs(DATA_PATH, exist_ok=True)
 
 GROUP_DATA_PATH = DATA_PATH / "groups.json"
 
