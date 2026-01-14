64809c54f51ab50c3ec3b0555ba993411d72adb8
Arthur Cabon <djarth2222@gmail.com>
2026-01-14 17:38:35 +0100

    feat: add cli command + cleaner UI

--- src/photobot/date.py
+++ src/photobot/date.py
@@ -2,8 +2,7 @@ from photobot.parameters import DATE_GROUP_DATA_PATH
 import pandas as pd
 import streamlit as st
 
-st.title("Photobot")
-st.subheader("Groupes par dates")
+# region HANDLE CSV
 
 existing_groups = pd.DataFrame(columns=["nom", "date_debut", "date_fin", "full_day"])
 if DATE_GROUP_DATA_PATH.exists() :
@@ -15,6 +14,35 @@ existing_groups["date_fin"] = pd.to_datetime(existing_groups["date_fin"])
 existing_groups["full_day"] = existing_groups["full_day"].astype("bool")
 
 existing_groups = existing_groups.sort_values("date_debut").reset_index(drop=True)
+
+# endregion
+
+# region UI
+
+st.title("Photobot")
+st.subheader("Groupes par dates")
+
+# region |---| Tweaks
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+        <style>
+            [data-testid="stToolbar"] {
+                display: none;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+        [data-testid="stMainBlockContainer"] {
+            padding-top: 2rem;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# endregion
 
 edited_df = st.data_editor(
     existing_groups,
@@ -39,8 +67,11 @@ edited_df = st.data_editor(
             "Journée entière"
         )
     },
-    width="stretch",   
+    width="stretch",
+    height=500
 )
 
 if st.button("Sauvegarder") :
     edited_df.to_csv(DATE_GROUP_DATA_PATH)
+
+# endregion
